a429c16674828ace7902a5f46ee0fa906b220b9d
mainagit <bnrdmaina3@gmail.com>
2023-01-02 01:50:07 -0800

    cor2: made my file excecutable

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * _isupper - A function that checks for uppercase character.
+ * _isupper - A function that checks for uppercase characters
  * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
-int _isupper(char c)
+int _isupper(int c)
 {
-  char uppercase = 'A';
-  int isupper = 0;
-   for (; uppercase <= 'Z'; uppercase++)
-    {
-      if (c == uppercase)
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
 	{
-	  isupper = 1;
-	  break;
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
 	}
-    }
-  return (isupper);
+	return (isupper);
 }
