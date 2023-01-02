820e84f615387dcbfafbedb1b7cf40bfd7ef7e60
mainagit <bnrdmaina3@gmail.com>
2023-01-02 06:02:30 -0800

    print_numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - outputs numbers from 1 to 9
+ *Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = '0';
+	for(; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
